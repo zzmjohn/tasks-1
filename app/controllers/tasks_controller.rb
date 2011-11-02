b773c0bac27f198658cc14d9609144f8ce169919
Jimmy Huang <jimmy.huang@duragility.com>
2011-11-01 20:12:22 -0700

    Completed Tasks Web App

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,31 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.new
     @tasks = Task.all
   end
 
   def create
 #    render :text => params.inspect
-    task = Task.create params[:task]
+    Task.create params[:task]
     redirect_to :back
+  end
+
+  def update
+    task = Task.find params[:id]
+
+    if task.update_attributes params[:task]
+      redirect_to tasks_path, :notice => "Your task has been successfully updated."
+    else
+      redirect_to :back, :notice => "There was an error updating your task."
+    end
+  end
+
+  def edit
+    @task = Task.find params[:id]
+  end
+
+  def destroy
+    Task.destroy params[:id]
+    redirect_to :back, :notice => "Task has been deleted."
   end
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe "Tasks" do
+  before do
+    @task = Task.create :task => "go to bed"
+  end
+
   describe "GET /tasks" do
     it "should display some tasks" do
-      @task = Task.create :task => "go to bed"
-
       visit tasks_path
       page.should have_content "go to bed"
     end
@@ -17,7 +19,44 @@ describe "Tasks" do
       current_path.should == tasks_path
       page.should have_content "go to work"
 
-      save_and_open_page
+#      save_and_open_page
+    end
+  end
+
+  describe "PUT /tasks" do
+    it "should describe a task" do
+      visit tasks_path
+      click_link "Edit"
+
+      current_path.should == edit_task_path(@task)
+
+      find_field("Task").value.should == "go to bed"
+
+      fill_in "Task", :with => "update task"
+      click_button "Update Task"
+
+      current_path.should == tasks_path
+      page.should have_content "update task"
+    end
+
+    it "should not update an empty task" do
+      visit tasks_path
+      click_link "Edit"
+
+      fill_in "Task", :with => ''
+      click_button "Update Task"
+
+      current_path.should == edit_task_path(@task)
+      page.should have_content "There was an error updating your task."
+    end
+  end
+
+  describe "DELETE /tasks" do
+    it "should delete a task" do
+      visit tasks_path
+      find("#task_#{@task.id}").click_link "Delete"
+      page.should have_content "Task has been deleted."
+      page.should have_no_content "go to bed"
     end
   end
 end
